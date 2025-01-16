4fdd07295767dda804cc3be7b7c8ab3fae341a67
Luv Valecha <139698035+Luv-valecha@users.noreply.github.com>
2025-01-16 18:56:13 +0530

    Initial setup

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectdb } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is up and running at http://localhost:${port}`);
+    connectdb();
+});
