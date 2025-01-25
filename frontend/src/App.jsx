65563c0c309a723b580a20e11f71c0df39f9d871
Luv Valecha <139698035+Luv-valecha@users.noreply.github.com>
2025-01-25 16:54:11 +0530

    frontend setup- tailwindcss, daisyui,axios and router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './components/HomePage.jsx'
+import LoginPage from './components/LoginPage.jsx'
+import ProfilePage from './components/ProfilePage.jsx'
+import SettingsPage from './components/SettingsPage.jsx'
+import SignUpPage from './components/SignUpPage.jsx'
+import Navbar from './components/Navbar.jsx'
+import { useAuthStore } from './store/useAuthStore.js'
+import { useEffect } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {authUser,checkAuth}= useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  },[]);
+
+  console.log({authUser});
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+        <Route path='/settings' element={<SettingsPage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+      </Routes>
     </>
   )
 }
