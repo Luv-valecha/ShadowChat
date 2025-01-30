e06b00ba94ae4613f9bf846048d31384b39ce927
Luv Valecha <139698035+Luv-valecha@users.noreply.github.com>
2025-01-30 23:44:55 +0530

    Added Chat Container UI and Implemented Message sending and updation feature

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set)=>({
+export const useChatStore = create((set,get)=>({
     messages:[],
     users:[],
     selectedUser:null,
@@ -15,7 +15,7 @@ export const useChatStore = create((set)=>({
             const res= await axiosInstance.get("/messages/users");
             set({users:res.data});
         }catch(error){
-            toast.error(error.response.data.message);
+            toast.error(error.response.data.error);
         }finally{
             set({isUsersLoading: false});
         }
@@ -24,12 +24,23 @@ export const useChatStore = create((set)=>({
     getMessages: async(userId)=>{
         set({isMessagesLoading:true});
         try {
-            const res= await axios.get(`/messages/${userId}`);
+            const res= await axiosInstance.get(`/messages/${userId}`);
             set({messages: res.data});
         } catch (error) {
-            toast.error(error.response.data.message);
+            toast.error(error.response);
         }finally{
             set({isMessagesLoading:false});
+        }
+    },
+
+    sendMessage :async(messageData)=>{
+        console.log(messageData);
+        const {selectedUser,messages} =get();
+        try {
+            const res= await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+            set({messages: [...messages,res.data]});
+        } catch (error) {
+            toast.error(error.response.data.error);
         }
     },
 
